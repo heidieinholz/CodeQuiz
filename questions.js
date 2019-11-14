aac4bb52e71a0da56290d5a56a7f6c6468495143
Heidi Einholz <heidein5@gmail.com>
2019-11-13 21:17:06 -0700

    Getting started

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "A procedure or function in a computer program that waits for an event to occur is called an _____________",
+      choices: ["div", "container", "event listener", "function"],
+      answer: "alerts"
+    },
+    {
+      title: "Array indexes start with _____",
+      choices: ["0", "1", "3.14", "potato"],
+      answer: "alerts"
+    },
+    {
+      title: "The return statement:",
+      choices: ["generates an error", "declares a variable", "exits a switch or loop", "stops the excution of the function and returns a value"],
+      answer: "alerts"
+    },
+];
+  
