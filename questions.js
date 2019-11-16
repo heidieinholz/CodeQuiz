879b2f35b780d579a06a72da966419d4bec5d1b9
Heidi Einholz <heidein5@gmail.com>
2019-11-15 23:08:52 -0700

    Updated HTML, CSS, and playing around with JS options

--- questions.js
+++ questions.js
@@ -1,28 +1,28 @@
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      question: "Commonly used data types DO NOT include:",
+      answers: ["strings", "booleans", "alerts", "numbers"],
+      correctAnswer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      question: "The condition in an if / else statement is enclosed within ____.",
+      answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      correctAnswer: "parentheses"
     },
     {
-      title: "A procedure or function in a computer program that waits for an event to occur is called an _____________",
-      choices: ["div", "container", "event listener", "function"],
-      answer: "alerts"
+      question: "A procedure or function in a computer program that waits for an event to occur is called an _____________",
+      answers: ["div", "container", "event listener", "function"],
+      correctAnswer: "event listener"
     },
     {
-      title: "Array indexes start with _____",
-      choices: ["0", "1", "3.14", "potato"],
-      answer: "alerts"
+      question: "Array indexes start with _____",
+      answers: ["0", "1", "3.14", "potato"],
+      correctAnswer: "0"
     },
     {
-      title: "The return statement:",
-      choices: ["generates an error", "declares a variable", "exits a switch or loop", "stops the excution of the function and returns a value"],
-      answer: "alerts"
+      question: "The return statement:",
+      answer: ["generates an error", "declares a variable", "exits a switch or loop", "stops the excution of the function and returns a value"],
+      correctAnswer: "stops the excution of the function and returns a value"
     },
 ];
   
