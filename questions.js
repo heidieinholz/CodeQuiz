8c7d23d746a49ff256a5926423fc53443eb35879
Heidi Einholz <heidein5@gmail.com>
2019-11-16 18:11:27 -0700

    Cleaned up index.html, restarting script.js

--- questions.js
+++ questions.js
@@ -1,28 +1,28 @@
 var questions = [
     {
-      question: "Commonly used data types DO NOT include:",
-      answers: ["strings", "booleans", "alerts", "numbers"],
-      correctAnswer: "alerts"
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
     },
     {
-      question: "The condition in an if / else statement is enclosed within ____.",
-      answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      correctAnswer: "parentheses"
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
     },
     {
-      question: "A procedure or function in a computer program that waits for an event to occur is called an _____________",
-      answers: ["div", "container", "event listener", "function"],
-      correctAnswer: "event listener"
+      title: "A procedure or function in a computer program that waits for an event to occur is called an _____________",
+      choices: ["div", "container", "event listener", "function"],
+      answer: "event listener"
     },
     {
-      question: "Array indexes start with _____",
-      answers: ["0", "1", "3.14", "potato"],
-      correctAnswer: "0"
+      title: "Array indexes start with _____",
+      choices: ["0", "1", "3.14", "potato"],
+      answer: "0"
     },
     {
-      question: "The return statement:",
-      answer: ["generates an error", "declares a variable", "exits a switch or loop", "stops the excution of the function and returns a value"],
-      correctAnswer: "stops the excution of the function and returns a value"
+      title: "The return statement:",
+      choices: ["generates an error", "declares a variable", "exits a switch or loop", "stops the excution of the function and returns a value"],
+      answer: "stops the excution of the function and returns a value"
     },
 ];
   
